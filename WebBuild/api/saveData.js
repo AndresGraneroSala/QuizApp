b7f2e1dcebef8afbe5cce472cc67b467b6a8bbed
AndresGS <andresnn21@gmail.com>
2023-12-15 12:46:49 +0100

    not try js

--- WebBuild/api/saveData.js
+++ WebBuild/api/saveData.js
@@ -2,12 +2,8 @@
 const fs = require('fs');
 
 module.exports = (req, res) => {
-  try {
     const data = req.body.data;
-    fs.writeFileSync('ruta/del/archivo/data.txt', JSON.stringify(data));
+    fs.writeFileSync('data.txt', JSON.stringify(data));
     res.status(200).send('Datos guardados correctamente en data.txt');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error al guardar los datos');
-  }
+
 };
